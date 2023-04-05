d920e259f4630c5c957f7d45972077bdbf5d5fdf
Muhammad Aqib <aqibafridi0345@gmail.com>
2023-04-05 13:19:47 +0500

    crud mutatin

--- pages/author/index.vue
+++ pages/author/index.vue
@@ -3,6 +3,72 @@
     <v-col cols="12" class="py-10">
       <PageHeader title="Authors" />
     </v-col>
+    <v-col cols="10" class="text-end"> 
+      <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+          >
+            +
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Add New Book</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Your full name"
+                    required
+                    v-model="author.full_name"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="your email address"
+                    required
+                    v-model="author.email"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click.prevent="addNewAuthor"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-col>
     <v-col cols="10">
       <v-data-table
         v-if="allAuthors"
@@ -13,23 +79,7 @@
       >
 
       </v-data-table>
-      <!-- <v-card variant="outlined" class="pa-8">
-        
-        <v-avatar>
-            <v-img
-                :src="singleAuthor.img"
-                :alt="singleAuthor.full_name"
-            ></v-img>
-        </v-avatar>
-
-        <v-card-title class="px-0"> {{ singleAuthor.full_name }} </v-card-title>
-
-        <v-card-actions class="pa-0">
-          <v-btn text class="blue--text" :to="`/books/${singleAuthor.id}`">
-            view book
-          </v-btn>
-        </v-card-actions>
-      </v-card> -->
+      
     </v-col>
   </v-row>
 </template>
@@ -37,6 +87,7 @@
 <script>
   import PageHeader from '@/components/utilities/PageHeader.vue';
   import FETCH_ALL_AUTHORS from "~/graphql/queries/authors/fetchAllAuthors.gql";
+  import ADD_NEW_AUTHOR from "~/graphql/queries/authors/addAuthor.gql";
 
   export default {
     name: 'InspirePage',
@@ -46,37 +97,32 @@
 
     data() {
       return {
-        authorBooks: [{
-            id: 1,
-            full_name: 'Aqib',
-            img: 'https://cdn.vuetifyjs.com/images/john.jpg'
-          },
-          {
-            id: 2,
-            full_name: 'Waqas',
-            img: 'https://cdn.vuetifyjs.com/images/john.jpg'
-          }
-        ],
+        dialog: false,
+        author: {
+          full_name: '',
+          email: '',
+
+        },
 
         headers: [
-        {
-          text: 'Id',
-          align: 'start',
-          sortable: false,
-          value: 'id',
-        },
-        {
-          text: 'Full Name',
-          sortable: false,
-          value: 'full_name',
-        },
-        {
-          text: 'Email Address',
-          sortable: false,
-          value: 'email',
-        },
-      ],
-      allAuthors: undefined,
+          {
+            text: 'Id',
+            align: 'start',
+            sortable: false,
+            value: 'id',
+          },
+          {
+            text: 'Full Name',
+            sortable: false,
+            value: 'full_name',
+          },
+          {
+            text: 'Email Address',
+            sortable: false,
+            value: 'email',
+          },
+        ],
+        allAuthors: undefined,
       }
     },
 
@@ -94,6 +140,26 @@
             console.log(".... Data not added ....")
             console.log(err);
           })
+      },
+
+      async addNewAuthor() {
+        const {data} = await $nuxt.$apollo.mutate({
+          mutation: ADD_NEW_AUTHOR,
+          variables: {
+            ...this.author
+          },
+        })
+        if(data.insert_author_one) {
+          this.dialog = false;
+          if(this.allAuthors.length > 0) {
+            this.allAuthors.push(data.insert_author_one)
+          }
+          else {
+            let data = [];
+            data.push(data.insert_author_one)
+            this.allAuthors = data
+          }
+        }
       }
     },
 
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,83 @@
     <v-col cols="12" class="py-10">
       <PageHeader title="Books" />
     </v-col>
+    <v-col cols="12" class="text-end"> 
+      <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+          >
+            +
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Add New Book</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Add title"
+                    required
+                    v-model="title"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Add isbn"
+                    required
+                    v-model="isbn"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Add avatar url"
+                    required
+                    v-model="avatar"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click.prevent="addNewBook"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-col>
     <v-col cols="12">
       <v-data-table
         v-if="allBooks"
@@ -29,29 +106,17 @@ import BaseCard from '../components/utilities/BaseCard.vue';
 import PageHeader from '../components/utilities/PageHeader.vue';
 // Graph ql Queries
 import FETCH_ALL_BOOKS from "~/graphql/queries/books/fetchAllBooks.gql";
+import ADD_NEW_BOOK from "~/graphql/queries/books/addNewBook.gql";
 
 export default {
   name: 'IndexPage',
 
   data() {
     return {
-      authorBooks: [
-        {
-          id: 1,
-          title: 'Ali Baba Chalis Chorr',
-          isbn: '987654321',
-          description: 'Hello,  Lorem ipsum dolor sit amet, consequat.',
-          author_id: 2
-        },
-        {
-          id: 2,
-          title: 'The human psyco',
-          isbn: '99999999',
-          description: 'Hello,  Lorem ipsum dolor sit amet, consequat.',
-          author_id: 1
-        },
-      ],
-
+      dialog: false,
+      title: '',
+      isbn: '',
+      avatar: '',
       headers: [
         {
           text: 'Id',
@@ -93,6 +158,30 @@ export default {
           console.log(".... Data not added ....")
           console.log(err);
         })
+    },
+
+    async addNewBook() {
+      // console.log(this.title, this.avatar, this.isbn)
+      const {data} = await $nuxt.$apollo.mutate({
+        mutation: ADD_NEW_BOOK,
+        variables: {
+          title: this.title,
+          isbn: this.isbn,
+          avatar: this.avatar,
+          author_id: 1,
+        },
+      })
+      if(data.insert_book_one) {
+        this.dialog = false;
+        if(this.allBooks.length > 0) {
+          this.allBooks.push(data.insert_book_one)
+        }
+        else {
+          let data = [];
+          data.push(data.insert_book_one)
+          this.allBooks = data
+        }
+      }
     }
   },
 
